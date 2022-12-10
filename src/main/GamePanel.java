7e384264f8af8be3edff588351d8d40904bbca03
Emanuele Simula <simulaemanuele@gmail.com>
2022-12-10 14:56:46 +0100

    Initial Commit and setting base components of the game

---
+++ src/main/GamePanel.java
@@ -0,0 +1,17 @@
+package main;
+
+import javax.swing.JPanel;
+import java.awt.Graphics;
+
+public class GamePanel extends JPanel {
+    
+    public GamePanel(){
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.fillRect(100, 100, 200, 50);
+    }
+}
